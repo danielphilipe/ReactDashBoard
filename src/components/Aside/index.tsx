a1e4dc68ccc9ab1d782496856e979835043a0029
Daniel Quintal <danielphilipe@hotmail.com>
2022-01-27 16:41:34 -0300

    Aside

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,11 +1,39 @@
 import React from "react";
-
-import {Container} from './styles' ;
+import logoImg from '../../assets/logo.svg';
+import {Container,Header,LogImg,MenuContainer,MenuItemLink,Tittle} from './styles' ;
+import {MdDashboard, MdArrowDownward,MdArrowUpward,MdExitToApp} from 'react-icons/md';
 
 const Aside: React.FC = () =>{
     return (
         <Container>
-            <h1>Aside</h1>
+
+            <Header>
+                <LogImg src={logoImg} alt="Logo Minha Carteira"/>
+                <Tittle>Minha carteira</Tittle>
+            </Header>
+           
+           
+           <MenuContainer>
+                <MenuItemLink href="#">
+                    <MdDashboard/>
+                    DashBoard
+                </MenuItemLink>
+
+                <MenuItemLink href="#">
+                    <MdArrowUpward/>
+                    Entradas
+                </MenuItemLink>
+
+                <MenuItemLink href="#">
+                    <MdArrowDownward/>
+                    Saidas
+                </MenuItemLink>
+
+                <MenuItemLink href="#">
+                    <MdExitToApp/>
+                    Sair
+                </MenuItemLink>
+           </MenuContainer>
         </Container>
     );
 }
