174b9658ed82c543d90b2f869339bcdc737df5da
Daniel Quintal <danielphilipe@hotmail.com>
2022-02-18 18:45:24 -0300

    finishing filters list

--- src/pages/Inout/index.tsx
+++ src/pages/Inout/index.tsx
@@ -1,17 +1,23 @@
 import React, {useMemo, useState, useEffect} from "react";
 
+import uuid from "uuidv4";
+
 import ContentHeader from '../../components/ContentHeader'
 import SelectInput from '../../components/SelectInput'
 import HistoryFinanceCard from '../../components/HistoryFinanceCard'
 import { useParams } from "react-router-dom";
 
+import listOfMonths from '../../Utils/months';
+
 import gains from '../../Repositories/gains';
 import expenses from '../../Repositories/expenses';
+import formatCurrency from '../../Utils/formatCurrency';
+import formatDate from '../../Utils/formatDate';
 
 import { Container, Content, Filters } from "./styles";
 
 interface IData {
-    id: number
+    id: string
     description: string;
     amountFormatted: string;
     frequency: string;
@@ -22,7 +28,9 @@ interface IData {
 const Inout: React.FC = () =>{
     
     const [data,setData] = useState<IData[]>([]);
-
+    const [monthSelected, setMonthSelected] = useState<string>(String(new Date().getMonth() + 1));
+    const [yearSelected, setYearSelected] = useState<string>(String(new Date().getFullYear()));
+    const [frequencySelected,setFrequencySelected] = useState(['recurrent','eventual']);
     const type = useParams();
 
     const props = useMemo(() => {
@@ -37,44 +45,93 @@ const Inout: React.FC = () =>{
         }
     },[type.type])
 
-    const month = [
-        {value: 1, label: 'Janeiro'},
-        {value: 2, label: 'Fevereiro'},
-        {value: 3, label: 'Março'}]
-    const year = [
-        {value: 2022, label: 2022}]
+    const months = useMemo(()=>{
+
+        return listOfMonths.map((month,index)=>{
+            return {
+                value: index +1,
+                label: month
+            };
+        })
+            
+        
+    },[props]);
+   
+        const year = useMemo(()=>{
+            let uniqueYears: number[] = [];
+            props.request.forEach(element => {
+                const date = new Date(element.date);
+                const year = date.getFullYear();
+
+                if(!uniqueYears.includes(year)){
+                    uniqueYears.push(year);
+                }
+            });
+
+            return uniqueYears.map(year => {
+                return {
+                    value: year,
+                    label: year
+                };
+            });
+
+        },[props]);
+
+        const handleFrequencyClick = (frequency: string) =>{
+            const alreadySelected = frequencySelected.findIndex(item=> item === frequency);
+
+            if(alreadySelected >= 0){
+                console.log('ja selecionou')
+            }
+        }
 
         useEffect(() =>{
-       const response = props.request.map(item =>{
-                return {
-                    id: Math.random()*data.length,
-                    description: item.description,
-                    amountFormatted: item.amount,
-                    frequency: item.frequency,
-                    dateFormatted: item.date,
-                    tagColor: item.frequency === 'recorrente' ? '#E44C4E' : '#4E41F0'
-                }
-            })
-            setData(response);
-        })
+       
+            const filteredDate = props.request.filter(item =>{
+            const date = new Date(item.date);
+            const month = String(date.getMonth() +1);
+            const year = String(date.getFullYear());
+
+            return month === monthSelected && year === yearSelected
+       });
+
+
+       const formattedDate = filteredDate.map(item=>{
+           
+        return {
+            id: uuid(),
+            description: item.description,
+            amountFormatted: formatCurrency(Number(item.amount)),
+            frequency: item.frequency,
+            dateFormatted: formatDate(item.date),
+            tagColor: item.frequency === 'recorrente' ? '#4E41F0':'#E44C4E'
+        }
+        });
+        setData(formattedDate);
+       
+        console.log(data);
+
+        },[props,monthSelected,yearSelected]);
 
     return (
         <Container>
             <ContentHeader title={props.title} lineColor={props.lineColor}>
-                <SelectInput options={month}/>
-                <SelectInput options={year}/>
+                <SelectInput options={months} onChange={(e) => setMonthSelected(e.target.value)} defaultValue={monthSelected}/>
+                <SelectInput options={year} onChange={(e) => setYearSelected(e.target.value)} defaultValue={yearSelected}/>
             </ContentHeader>
 
             <Filters>
                 <button 
                 type="button"
-                className="tag-filter tag-filter-recurrent">
+                className="tag-filter tag-filter-recurrent"
+                onClick={() => handleFrequencyClick('recurrent')}>
                     Recorrentes
                 </button>
 
                 <button 
                 type="button"
-                className="tag-filter tag-filter-eventual">
+                className="tag-filter tag-filter-eventual"
+                onClick={() => handleFrequencyClick('eventual')}>
 
                     Eventuais
                 </button>
